bfa96450a299688b4f627f88e28155dba7311d27
MitulP91 <mitulp91@gmail.com>
2013-11-05 22:05:11 -0500

    add blobs; blob animation

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -6,7 +6,7 @@ var app = {
 		app.height = document.height
 
 		app.colors = ["#4F4046","#8CBEB2","#EBE4B3","#F3B557","#F05B47","#4F4046","#8CBEB2","#EBE4B3","#F3B557","#F05B47"];
-
+		app.blobs = []
 
 		app.stage = new Kinetic.Stage({
 			container: "main_container",
@@ -15,65 +15,74 @@ var app = {
 		})
 		app.layer = new Kinetic.Layer();
 
-		app.make.circles2(app.width/2, 20)
-		app.make.squares(app.width, app.height, 20)
-		app.make.circles3(app.width/2, 20)
-		app.make.circles(app.width/2, 20)
+		app.make.blobs(0)
 	},
 
 	make: {
+		blobs: function(count, step) {
+			if(count > 5) {
+				app.stage.add(app.layer)
+				var period = 2000;
+	      var centerTension = 0;
+	      var amplitude = 1;
+
+	      var anim = new Kinetic.Animation(function(frame) {
+	      	for(var n = 0; n < app.blobs.length; n++) {
+	          app.blobs[n].setTension(amplitude * Math.sin(frame.time * 2 * Math.PI / period) + centerTension);
+	        }
+	      }, app.layer); 
+
+	      anim.start();
+			} else {
+				var points = [];
+        for(var i = 0; i < 5; i++) {
+          points.push({
+            x: (app.width) * Math.random(),
+            y: (app.height) * Math.random()
+          });
+        }
+
+				var blob = new Kinetic.Blob({
+					points: points,
+					fill: app.colors[Math.floor(Math.random()*10)],
+					stroke: app.colors[Math.floor(Math.random()*10)],
+					strokeWidth: 2,
+					tension: 0,
+					opacity: Math.random(),
+					draggable: true
+				})
+				app.layer.add(blob)
+				app.blobs.push(blob)
+
+	      return app.make.blobs(count + 1)
+			}
+		},
 		circles: function(radius, step){
 			if(radius <= 0) {
 				app.stage.add(app.layer)
 			} else {
 				var circle = new Kinetic.Circle({
-					x: 0,
-					y: 0,
+					x: (Math.random()*app.width),
+					y: (Math.random()*app.height),
 					radius: radius,
-					stroke: "black",
+					stroke: app.colors[Math.floor(Math.random()*10)],
 					strokeWidth: 2,
-					fill: app.colors[Math.floor(Math.random()*10)],
-					draggable: true
+					fill: app.colors[Math.floor(Math.random()*10)]
 				})
 				app.layer.add(circle)
+				var amplitude = Math.random()*150;
+		    var period = 2000;
+		    // in ms
+		    var centerX = app.width / 2;
+
+		    var anim = new Kinetic.Animation(function(frame) {
+		      circle.setX(amplitude * Math.sin(frame.time * 2 * Math.PI / period) + centerX);
+		    }, app.layer);
+
+	      anim.start();
 				return app.make.circles(radius - step, step)
 			}
 		},
-		circles2: function(radius, step){
-			if(radius <= 0) {
-				app.stage.add(app.layer)
-			} else {
-				var circle = new Kinetic.Circle({
-					x: app.width / 2,
-					y: app.height / 2,
-					radius: radius,
-					stroke: "black",
-					strokeWidth: 2,
-					fill: app.colors[Math.floor(Math.random()*10)],
-					draggable: true
-				})
-				app.layer.add(circle)
-				return app.make.circles2(radius - step, step)
-			}
-		},
-		circles3: function(radius, step){
-			if(radius <= 0) {
-				app.stage.add(app.layer)
-			} else {
-				var circle = new Kinetic.Circle({
-					x: app.width,
-					y: app.height,
-					radius: radius,
-					stroke: "black",
-					strokeWidth: 2,
-					fill: app.colors[Math.floor(Math.random()*10)],
-					draggable: true
-				})
-				app.layer.add(circle)
-				return app.make.circles3(radius - step, step)
-			}
-		},
-
 		squares: function(width,height,step){
 			
 			if (width <= 0 || height <= 0) {
@@ -86,8 +95,7 @@ var app = {
 					height: height,
 					stroke: "black",
 					strokeWidth: 2,
-					fill: app.colors[Math.floor(Math.random()*10)],
-					draggable: true
+					fill: app.colors[Math.floor(Math.random()*10)]
 				})
 				app.layer.add(box)
 				return app.make.squares(width-step,height-step,step)
